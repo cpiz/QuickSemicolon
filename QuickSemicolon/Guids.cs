ea69cce0a64dc02cfc7a685b150619146cc3db73
caijw-yy-cygwin <caipiz@gmail.com>
2014-10-16 20:41:08 +0800

    VS2013插件for C++/C#，让光标快速跳至行尾并增加分号符（如果需要），结束当前语句

---
+++ QuickSemicolon/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace cpiznet.QuickSemicolon
+{
+    static class GuidList
+    {
+        public const string guidQuickSemicolonPkgString = "d378e555-224f-46b3-98ab-0bd4713d754c";
+        public const string guidQuickSemicolonCmdSetString = "a62273f5-0f74-4226-9b46-e8bcca1fbcc5";
+
+        public static readonly Guid guidQuickSemicolonCmdSet = new Guid(guidQuickSemicolonCmdSetString);
+    };
+}
---
+++ QuickSemicolon/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace cpiznet.QuickSemicolon
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidQuickSemicolon =        0x100;
+
+
+    };
+}
